41b8629bbf0c7ec57606bc486c84f7b9eadd8132
Tina Topka <anon.justch1ll1n@gmail.com>
2024-05-11 23:53:14 +0300

    added login, logout, register check(i think that's what i've done)
    also blank html files

--- djangodb/djangodb/settings.py
+++ djangodb/djangodb/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'website.User'
+AUTHENTICATION_BACKEND = ['website.backends.EmailBackend']
--- djangodb/website/models.py
+++ djangodb/website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class Vacancy(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+# class Item(models.Model):
+#     vacancy = models.ForeignKey(Vacancy, on_delete=models.CASCADE)
+#
