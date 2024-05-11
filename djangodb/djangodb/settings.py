fcea8b47c4a398da4e8bdb89ebb4df0479c85395
Tina Topka <anon.justch1ll1n@gmail.com>
2024-05-12 00:16:43 +0300

    updated the last commit and fixed some errors

--- djangodb/djangodb/settings.py
+++ djangodb/djangodb/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
+    'users',
     'dashboard',
 ]
 
@@ -124,5 +125,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'website.User'
-AUTHENTICATION_BACKEND = ['website.backends.EmailBackend']
+AUTH_USER_MODEL = 'users.User'
+AUTHENTICATION_BACKEND = ['users.backends.EmailBackend']
---
+++ djangodb/users/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from users.models import User
+from .forms import RegisterUserForm
+
+
+# register applicant only
+def register_applicant(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            var = form.save(commit=False)
+            var.is_applicant = True
+            var.username = var.email
+            var.save()
+            messages.info(request, 'Акаунт створений')
+            return redirect('login')
+        else:
+            messages.warning(request, 'Шось не то')
+            return redirect('register-applicant')
+    else:
+        form = RegisterUserForm()
+        context = {'form': form}
+        return render(request, 'users/register_applicant.html', context)
+
+
+# register recruiter only
+def register_recruiter(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            var = form.save(commit=False)
+            var.is_recruiter = True
+            var.username = var.email
+            var.save()
+            messages.info(request, 'Акаунт створений')
+            return redirect('login')
+        else:
+            messages.warning(request, 'Шось не то')
+            return redirect('register-recruiter')
+    else:
+        form = RegisterUserForm()
+        context = {'form': form}
+        return render(request, 'users/register_recruiter.html', context)
+
+
+# login a user
+def login_user(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=email, password=password)
+        if user is not None and user.is_active:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.warning(request, 'Шось не то')
+            return redirect('login')
+    else:
+        return render(request, 'users/login.html')
+
+
+# logout a user
+def logout_user(request):
+    logout(request)
+    messages.info(request, 'Ви вийшли з акаунта')
+    return redirect('login')
