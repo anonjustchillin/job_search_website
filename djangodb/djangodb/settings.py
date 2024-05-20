dc348d0f4f454b814559d0d5253eeb8be377a7fe
Tina Topka <anon.justch1ll1n@gmail.com>
2024-05-20 03:56:12 +0300

    new: home guest page, log in and registration pages

--- djangodb/djangodb/settings.py
+++ djangodb/djangodb/settings.py
@@ -49,7 +49,12 @@ INSTALLED_APPS = [
     'job',
 
     'django_filters',
+    # 'crispy_forms',
+    # 'crispy_bootstrap5',
 ]
+
+# CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+# CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -127,7 +132,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'staticfiles'
+]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
