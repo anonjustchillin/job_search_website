579516f7c1fd181c8da49a9f562b349397b2292d
Анонімус Анонімко <127945507+anonjustchillin@users.noreply.github.com>
2024-06-16 03:02:19 +0300

    Update settings.py

--- jobfinder/jobfinder/settings.py
+++ jobfinder/jobfinder/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@eu4cll7rkb5oesi9k&yw#n1)@%6%&tdp-wqh%7vc4()3+3-s2'
+SECRET_KEY = '...'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
